aedb6e96456fe7e9da243ff0528ad66c730eb7e1
tsakshi01 <tsakshi01@gmail.com>
2023-11-15 00:02:46 -0800

    Display Posts from Database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
 import './Firebase.js';
+import './FoodPosts.js';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { signInWithGoogle } from './Firebase.js';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { db } from './Firebase.js'
+import {getDocs, collection, addDoc} from 'firebase/firestore'
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "Food_Post")
+
+  //NEW POST STATES
+  const [newTitle, setTitle] = useState(null);
+  const [newImage, setImage] = useState('');
+  const [newText, setText] = useState('');
+
+  const getPostList = async () => {
+    try{
+      const data = await getDocs(postCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(), 
+        id: doc.id,
+      }));
+      setPostList(filteredData)
+      console.log(filteredData);
+    } catch (err) {
+      console.error(err);
+    };
+  }
+  useEffect(() => {
+    getPostList();
+  }, [])
+
+  const onSubmitPost = async () => {
+    try{
+      await addDoc(postCollectionRef, {
+        title: newTitle, 
+        text: newText, 
+        image: newImage,
+      });
+      getPostList();
+    }catch(err){
+      console.error(err);
+    }
+  }
 
   const handleSignIn = async () => {
     try {
@@ -34,6 +74,7 @@ const App = () => {
       setUser(null);
     }
   });
+   
 
   return (
     <div>
@@ -42,6 +83,24 @@ const App = () => {
           <p>Welcome, {user.displayName}!</p>
           <p>Email: {user.email}</p>
           <button onClick={handleSignOut}>Sign Out</button>
+
+          <div>
+            <input type="text" placeholder="Title....." onChange={(e) => {console.log(e.target.value);}}/>
+            <input type="text" placeholder="Text...."/>
+            <input type="text" placeholder="Image"/>
+            <input type="checkbox"/>
+            <label>Veg</label>
+            <button onClick={onSubmitPost}>Submit Post</button>
+          </div>
+          <div>
+            {postList.map((post, index) => (
+              <div key={index}>
+                <h1>{post.title}</h1>
+                <img src={post.image} width="100" height="100"></img>
+                <p>{post.text}</p>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <button onClick={handleSignIn}>Sign in with Google</button>
@@ -51,3 +110,7 @@ const App = () => {
 };
 
 export default App;
+
+/*<input placeholder = "Image...." value={newImage} onChange={(e) => setImage(e.target.value)}/>
+            <input placeholder = "Title..." value={newTitle} onChange={(e) => setTitle(e.target.value)}/>
+            <input placeholder = "Text....." value={newText} onChange={(e) => setText(e.target.value)}/> */
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,39 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import {getDatabase, ref, set} from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-import {FIREBASE_API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, SENDER_ID, APP_ID, MEASUREMENT_ID} from './info.js';
 
-
-// temporarily using because can't set up .env
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-/*
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
-*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const firestore = getFirestore(app); 
-// const db = app.firestore();
+export const db = getFirestore(app);
 
 const provider = new GoogleAuthProvider();
   
@@ -54,17 +37,3 @@ const provider = new GoogleAuthProvider();
   // Define localStorage earlier in your code
   const localStorage = window.localStorage;
 
-/*function getUserInfo(user_name ,name, contact_info, userId)
-{
-  const d = getDatabase();
-  const reference = ref(d, 'user info/' + );
-
-  set(reference, {
-    name: name,
-    user_name: user_name,
-    contact_info: contact_info,
-    userId: userId
-  });
-}
-
-userInfo()*/
