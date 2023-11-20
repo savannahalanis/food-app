fb234340e87108f74943d0991730ded34f1b7659
applesunn <22aprils@students.harker.org>
2023-11-20 15:30:30 -0800

    Post Framework
    Post.css - how a post appears
    Post.js - page to create a new post

---
+++ src/pages/Post.js
@@ -0,0 +1,35 @@
+
+function Picture()
+{
+
+}
+
+function Review()
+{
+
+}
+
+function RestaurantTag()
+{
+
+}
+
+function Restrictions()
+{
+
+}
+
+//comments, time, date, likes
+
+function PostButton()
+{
+
+}
+
+
+export default function Post() {
+    return (
+      <>
+      </>
+    )
+}
