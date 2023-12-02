0a3674059b80eb97d59908cdf1444e8fb95448dc
Savannah Alanis <savannahalanis2004@gmail.com>
2023-12-02 15:45:03 -0800

    Marketplace posts now display username of the creator

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import MarketplacePost from './MarketplacePosts.js'
 
 import React, { useEffect, useState } from 'react';
 import { getAuth, getAdditionalUserInfo, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { getDocs, addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { getDocs, addDoc, collection, serverTimestamp, query, where } from 'firebase/firestore';
 import { db } from './Firebase.js'
 
 const App = () => {
   const [userList, setUserList] = useState([]);
   const [user, setUser] = useState(null);
+  const [userDocID, setUserDocID] =  useState(null);
   const usersCollectionRef = collection(db, 'Users');
 
   const getUserList = async () => {
@@ -48,6 +49,17 @@ const App = () => {
     }
   };
 
+  const getUserDocID= async (uid) => {
+    const querySnapshot = await getDocs(query(collection(db, "Users"), where("uid", "==", uid)));
+    if(!querySnapshot) {
+      console.log("nothing in getUserDocID() snapshot")
+      return;
+    }
+    const id = querySnapshot.docs[0].id;
+    setUserDocID(id);
+    return;
+  };
+
   const handleSignIn = async () => {
     console.log("Here");
     try {
@@ -60,10 +72,12 @@ const App = () => {
       if(isNewUser){
         addUser(user);
       }
+      getUserDocID(user.uid)
     } catch (error) {
       console.error('Error signing in:', error);
     }
   };
+
   const auth = getAuth();
   const handleSignOut = () => {  
     auth.signOut().then(() => {
@@ -90,7 +104,7 @@ const App = () => {
           <p>Email: {user.email}</p>
           <button onClick={handleSignOut}>Sign Out</button>
           <FoodPost/>
-          <MarketplacePost/>
+          <MarketplacePost userID = {userDocID}/>
         </div>
       ) : (
         <button onClick={handleSignIn}>Sign in with Google</button>
--- src/MarketplacePosts.js
+++ src/MarketplacePosts.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { db } from './Firebase.js'
-import {getDocs, collection, addDoc, deleteDoc, updateDoc, doc, serverTimestamp, Timestamp, orderBy, query, where, direction} from 'firebase/firestore'
+import {getDocs, getDoc, collection, addDoc, deleteDoc, updateDoc, doc, serverTimestamp, Timestamp, orderBy, query, limit, where, direction, getFirestore} from 'firebase/firestore'
 // TODO: update contact information to grab from user
 // TODO: update hours to select hours
 // TOOD: update user to automatically be a user
 // TODO: update it to display user
 // TODO: update date to not allow to pick past dates/times
 
-const MarketplacePost = () => {
+const MarketplacePost = ({ userID }) => {
+
     const [marketplaceList, setMarketplaceList] = useState([]);
     const [filterBy, setFilterBy] = useState("date")
     const marketplaceCollectionRef = collection(db, "Selling_Post")
@@ -18,33 +19,30 @@ const MarketplacePost = () => {
     const [newStartHour, setStartHour] = useState('');
     const [newEndHour, setEndHour] = useState('');
     const [newPrice, setPrice] = useState('');
-    const [newUser, setUser] = useState('');
-    // idk abt this one
-    const [newLocations, setLocations] = useState('');
-    const [newDate, setDate] = useState('');
 
-    const getMarketplaceList = async () => {        
-        try {  
+    const getMarketplaceList = async () => {
+        try {
             const data = await getDocs(query(collection(db, "Selling_Post"), where("date", ">=", currentDate()), orderBy(filterBy)));
-            const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-            setMarketplaceList(filteredData);
+            const posts = data.docs.map(async (doc) => {
+                const postData = { ...doc.data(), id: doc.id };
+                postData.userName = await getNameFromID(postData.uid);
+                return postData;
+            });
+    
+            const resolvedPosts = await Promise.all(posts);
+            setMarketplaceList(resolvedPosts);
         } catch (error) {
             console.error('Error fetching marketplace data:', error);
         }
     };
+
 
     useEffect(() => {
         getMarketplaceList();
     }, []);
 
     const handleFilter = () => {
-        console.log('Search button clicked with filter:', filterBy);
         if (filterBy === 'date') {
-            /*
-            console.log("date hit again")
-            const sortedByDate = [...marketplaceList].sort((a, b) => new Date(a.date) - new Date(b.date));
-            setMarketplaceList(sortedByDate);
-            */
             getMarketplaceList();
         } else if (filterBy === 'price') {
             const sortedByPrice = [...marketplaceList].sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
@@ -53,6 +51,7 @@ const MarketplacePost = () => {
     };
 
     const onSubmitMarketplacePost = async () => {
+
         try{
             if (newEndHour < newStartHour) {
                 alert("Start time must be before end time");
@@ -65,16 +64,10 @@ const MarketplacePost = () => {
             }
       
             function removeTrailingZeros(numberString) {
-                console.log(typeof numberString)
-                console.log("hit function")
-                console.log(numberString)
                 var periodPos = -1;
                 if (numberString.indexOf(".") !== -1) {
                     var decimalPlaces = numberString.length - numberString.indexOf(".") -1;
-                    console.log("hit . in string");
-                    console.log(periodPos);
                 } else {
-                    console.log("hit . not in string");
                     return numberString + ".00";
                 }
             
@@ -82,25 +75,21 @@ const MarketplacePost = () => {
 
                 if (decimalPlaces == 1) {
                     zeroString = "0";
-                    console.log("zero string 0");
                 } else if (decimalPlaces == 0 || decimalPlaces == 0)  {
                     zeroString = "00";
-                    console.log("zero string 00");
                 }
-
-                console.log(numberString + zeroString)
 
                 return numberString + zeroString;
             }
-            
 
+  
             await addDoc(marketplaceCollectionRef, {
                 contactInfo: newContactInfo,
                 contactType: newContactType,
                 startHour: newStartHour,
                 endHour: newEndHour,
                 price: removeTrailingZeros(newPrice),
-                user: newUser,
+                uid: userID,
                 date: Timestamp.fromDate(new Date())
             });
 
@@ -117,10 +106,32 @@ const MarketplacePost = () => {
         const currentDate = new Date();
         currentDate.setHours(0, 0, 0, 0); 
         const currentDateTimestamp = Timestamp.fromDate(currentDate);
-        console.log(currentDateTimestamp.toDate());
         return currentDateTimestamp;        
-    }
+    };
 
+
+    const getNameFromID = async(userID) => {
+
+        try {
+            const userDocRef = doc(collection(db, 'Users'), userID);
+            const userDoc = await getDoc(userDocRef);
+
+            if (!userDoc.exists) {
+                return "User not found";
+            }
+
+            const username = userDoc.data().displayName;
+
+            return username;
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
+
+
+    
     return(
         <div>
             <div>
@@ -140,7 +151,6 @@ const MarketplacePost = () => {
                 <input type="time" required placeholder="Start Hour" onChange={(e) => {setStartHour(e.target.value)}}/>
                 <input type="time" required placeholder="End Hour" onChange={(e) => {setEndHour(e.target.value)}}/>
                 <input type="number" required placeholder="Price" pattern="^\d*(\.\d{0,2})?$" onChange={(e) => {setPrice(e.target.value)}}/>
-                <input type="text" required placeholder="User ID" onChange={(e) => {setUser(e.target.value)}}/>
                 <button onClick={onSubmitMarketplacePost}>Submit Marketplace Post</button>
             </div>
             <div>
@@ -148,16 +158,14 @@ const MarketplacePost = () => {
                 {marketplaceList.map((post, index) =>
                     <div key={index}>
                         <p>
-                            user = {post.user}<br />
+                            user = {post.userName}<br />
+
                             contact info ({post.contactType}) = {post.contactInfo}<br />
                             from time {post.startHour}-{post.endHour}<br />
                             at ${post.price} <br />
-                        {post.date && (
-                            <span>
-                                Date: {post.date.toDate().toLocaleDateString()} 
-                                {/* Assuming post.date is a Timestamp object */}
-                            </span>
-                        )}
+                            {post.date && (
+                                <span> Date: {post.date.toDate().toLocaleDateString()} </span>
+                            )}
                         </p>
                     </div>
                 )}
