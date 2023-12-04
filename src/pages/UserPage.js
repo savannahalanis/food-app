b40019db23809ce0a8813096d3381a33d460caab
Megan <meganjacob2004@yahoo.com>
2023-12-04 00:39:57 -0800

    Update user profile to display signed in user's name, follower count, and following count

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -5,6 +5,13 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { Grid, Paper, Typography } from '@mui/material';
 import Navbar from "../components/Navbar"
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+ import { db } from '../Firebase.js'
+ import { getDocs, collection, updateDoc, doc, getDoc, where, query } from 'firebase/firestore';
+
+
 
 function Name({name})
 {
@@ -14,6 +21,7 @@ function Name({name})
 }
 
 function Posts() {
+   
   return (
      <>
         <ImageList sx={{ width: 900, height: 750 }} cols="3" >
@@ -68,6 +76,46 @@ function Following({followingcount})
 
 
 export default function UserPage() {
+   const location = useLocation();
+  const { state } = location;
+  // const { user } = state || {};
+  // const parsedUser = user ? JSON.parse(user) : null;
+  const [user, setUser] = useState(null);
+  const userCollection = collection(db, "Users")
+  const auth = getAuth();
+  const [userData, setUserData] = useState(null);
+
+
+  onAuthStateChanged(auth, async(user) => {
+    if (user) {
+      setUser(user);
+      const userQuery = query(collection(db, "Users"), where("uid", "==", user.uid));
+      const userQuerySnapshot = await getDocs(userQuery);
+
+      const userDoc = doc(db, "Users", userQuerySnapshot.docs[0].id);
+      const userSnapshot = await getDoc(userDoc);
+      setUserData(userSnapshot.data());
+    } else {
+      setUser(null);
+    }
+  });
+  console.log(user);
+
+  useEffect(() => {
+   const unsubscribe = onAuthStateChanged(auth, (user) => {
+     if (user) {
+       setUser(user);
+     } else {
+       setUser(null);
+     }
+   });
+ 
+   // Cleanup the subscription when the component unmounts
+   return () => unsubscribe();
+ }, [auth]);
+ 
+ console.log(userData)
+
    return (
       <>
       <Navbar></Navbar>
@@ -76,17 +124,17 @@ export default function UserPage() {
       <Grid container spacing={2} class = "center rowcontainer" >
          <Grid item xs={2}>
             <Typography variant="h6" align="left">
-               <Name name="April Sun" />
+               <Name name={user?.displayName || 'Unknown User'} />
             </Typography>
          </Grid>
          <Grid item xs={1} sx={{marginTop:"20px"}}>
          <NumPosts numPosts="2" />
         </Grid>
         <Grid item xs={1} sx={{marginTop:"20px"}}>
-         <Followers followercount="0" />
+         <Followers followercount={userData?.followers?.length || 0} />
         </Grid>
         <Grid item xs={1} sx={{marginTop:"20px"}}>
-         <Following followingcount="0" />
+         <Following followingcount={userData?.following?.length || 0} />
         </Grid>
       </Grid>
 
